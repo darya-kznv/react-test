783cb3b6e07a4f62cb7204f0b9675649fbc5868f
Darya <floverika@gmail.com>
2021-02-25 08:02:30 +0300

    add output css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,70 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 require('babel-polyfill');
 
-module.exports = {
-    entry: {
-        index: ['babel-polyfill', './src/index.js']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.[hash].js',
-    },
-    devServer: {
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    // {
-                    //     loader: MiniCssExtractPlugin.loader,
-                    //     options: {
-                    //         publicPath: '/dist',
-                    //     },
-                    // },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                config: path.resolve(__dirname, 'postcss.config.js'),
+module.exports = ( env, argv ) => {
+    var isDevelopmentMode = ( argv.mode === "development" );
+
+    return({
+        entry: {
+            index: ['babel-polyfill', './src/index.js']
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'index.[hash].js',
+        },
+        devServer: {
+            historyApiFallback: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        isDevelopmentMode ? 'style-loader' : {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                publicPath: '/dist',
+                            }
+                        },
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                postcssOptions: {
+                                    config: path.resolve(__dirname, 'postcss.config.js'),
+                                },
                             },
                         },
-                    },
 
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        }),
-        // new MiniCssExtractPlugin({
-        //     filename: 'index.css',
-        // }),
-    ]
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'index.css',
+            }),
+
+        ],
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin({
+                    test: /\.js(\?.*)?$/i,
+                }),
+            ]
+        },
+    })
 };
